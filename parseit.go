5d8d8acdc5b5e195b6ca2f85bd757aa9519ef83b
Nicholas Donnelly <ndonnelly1006@gmail.com>
2016-04-14 16:06:31 +0200

    +Added ability to save report to external file. *Changed the way reports are printed both to console and external files. It is now much prettier.

--- parseit.go
+++ parseit.go
@@ -4,7 +4,9 @@ import(
     //"log"
     "fmt"
     "flag"
+    "strings"
     "os"
+    "bufio"
     "github.com/dotabuff/manta"
 )    
 
@@ -12,6 +14,8 @@ var modePtr = flag.String("mode", "text", "The mode of output for this match.")
 var matchPtr = flag.String("match", "", "The match file you would like to parse")
 //var offlinePtr = flag.Bool("offline", false, "Set this to true if you want the match to be downloaded directly instead of found in the current directory.")
 var heroPtr = flag.String("hero", "", "Use this to select the hero you would like to parse data for (only for image mode). Use full names, all lowercase. Separate separate words using a dash character (-).")
+var outputFilePtr = flag.Bool("save-external", false, "Use this to save the text report as an external file instead of only printing it to console.")
+var outputFilenamePtr = flag.String("output-file", "", "This flag specifies a custom filename for text reports. By default they will simply be named with the match ID. Include a file extension.")
 
 
 var heroes [10]Hero
@@ -39,6 +43,12 @@ func main(){
     switch *modePtr{
     case "text":
         textParse()
+        printTextReport()
+        if *outputFilePtr && *outputFilenamePtr == ""{
+            saveTextReport(strings.TrimSuffix(*matchPtr, ".dem") + ".txt")
+        }else if *outputFilePtr{
+            saveTextReport(*outputFilenamePtr)
+        }
     case "image":
         if *heroPtr == ""{
             fmt.Println("You selected image mode but didn't provide a hero. Please include the --hero=hero_name flag and try again.")
@@ -70,7 +80,6 @@ func textParse(){
     fmt.Println("Starting parser...")
     parser.Start()
     fmt.Println("Parsing complete. Printing report.")
-    printTextReport()
 }
 
 func imageParse(){
@@ -81,7 +90,38 @@ func imageParse(){
 
 func printTextReport(){
     chatResultKeys := GetAlphabetizedKeyListFromMap(ChatResult)
+    fmt.Println("+---------------------------------+")
     for k := range chatResultKeys{
-        fmt.Println(GetPrintableStringFromVariableName(chatResultKeys[k]), ": ", ChatResult[chatResultKeys[k]])
+        s := "| "
+        s += fmt.Sprintf("%24v: ", GetPrintableStringFromVariableName(chatResultKeys[k]))
+        s += fmt.Sprintf("%05v |\n", ChatResult[chatResultKeys[k]])
+        fmt.Println(s)
+    }
+    fmt.Println("+---------------------------------+")
+}
+
+func saveTextReport(filename string){
+    f, err := os.Create(filename)
+    check(err)
+    defer f.Close()
+    writer := bufio.NewWriter(f)
+    chatResultKeys := GetAlphabetizedKeyListFromMap(ChatResult)
+    writer.WriteString("+---------------------------------+\n")
+    for k:= range chatResultKeys{
+        s := "| "
+        s += fmt.Sprintf("%24v: ", GetPrintableStringFromVariableName(chatResultKeys[k]))
+        s += fmt.Sprintf("%05v |\n", ChatResult[chatResultKeys[k]])
+        writer.WriteString(s)
+    }
+    writer.WriteString("+---------------------------------+")
+    f.Sync()
+    writer.Flush()
+    fmt.Println("Printed report into " + filename + ".")
+}
+
+
+func check(e error) {
+    if e != nil {
+        panic(e)
     }
 }
